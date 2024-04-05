9f4dc18d81dbefa3ca2a3a75ceeab5138048bcde
dod0lp <dodotv007@gmail.com>
2024-04-05 18:11:15 +0200

    Added finding out if i have option name -t present

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 // -t shows names of files/folders in the archive
+
+bool is_t_option(const char* option) {
+    if (strcmp(option, "-t") == 0) {
+        return true;
+    }
+    return false;
+}
 
 void t_option(const char* archive_name) { 
     FILE *file = fopen(archive_name, "rb");
@@ -12,10 +20,13 @@ void t_option(const char* archive_name) {
 }
 
 int main(int argc, char* argv[]) {
-    printf("Number of arguments: %d\n", argc);
+    // printf("Number of arguments: %d\n", argc);
 
     for (int i = 0; i < argc; i++) {
-        printf("Argument %d: %s\n", i, argv[i]);
+        // printf("Argument %d: %s\n", i, argv[i]);
+        if (is_t_option(argv[i])) {
+            printf("option is -t %s %d", argv[i], i);
+        }
     }
     return 0;
 }
