dbb4befb1bebec047fe784d9b13dc8d56ff38297
dod0lp <dodotv007@gmail.com>
2024-04-05 18:23:26 +0200

    Added some way of saving option values and see if they were set

--- src/main.c
+++ src/main.c
@@ -3,7 +3,24 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define MAX_OPTION_SIZE 100
+#define NUM_KEYS 4
+
 // -t shows names of files/folders in the archive
+
+char Options[NUM_KEYS] = { 't', 'f', 'x', 'v' };
+
+typedef struct {
+    char value[MAX_OPTION_SIZE + 1];
+    bool is_set;
+} Values;
+
+void initializeValues() {
+    for (int i = 0; i < NUM_KEYS; i++) {
+        values[i].value[MAX_OPTION_SIZE] = '\0';
+        values[i].is_set = false;
+    }
+}
 
 bool is_t_option(const char* option) {
     if (strcmp(option, "-t") == 0) {
@@ -25,7 +42,7 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < argc; i++) {
         // printf("Argument %d: %s\n", i, argv[i]);
         if (is_t_option(argv[i])) {
-            printf("option is -t %s %d", argv[i], i);
+            printf("option is %s %d", argv[i], i);
         }
     }
     return 0;
