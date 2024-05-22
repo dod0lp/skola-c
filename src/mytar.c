0c41fc982ef8551d914946ef9fc73991e9beffad
dod0lp <dodotv007@gmail.com>
2024-05-22 05:01:18 +0200

    Can find name of archive name by -f

--- src/mytar.c
+++ src/mytar.c
@@ -5,6 +5,7 @@
 
 #define MAX_OPTION_SIZE 50
 #define NUM_OPTIONS_STRINGS 10
+#define MAX_ARCHIVENAME_LEN 50
 #define NUM_OPTIONS 4
 
 // -t List the contents of an archive. Arguments are optional.
@@ -20,9 +21,9 @@ static bool OptionsSet[26] = { false }; // make true OptionSet[char] to show tha
 // TODO: fix arrays below to properly know which option needs no value/only 1value etc so there
 //       is not space allocated for no reason
 char option_f_values[NUM_OPTIONS_STRINGS][MAX_OPTION_SIZE];
-// char option_t_values[NUM_OPTIONS_STRINGS];
-// char option_x_values[NUM_OPTIONS_STRINGS];
-// char option_v_values[NUM_OPTIONS_STRINGS];
+// char option_t_values
+// char option_x_values
+// char option_v_values
 
 void init_option_values() {
     // TODO: Fill arrays with zeros or something
@@ -34,7 +35,7 @@ char is_valid_option(const char* option) {
     }
     for (int i = 0; i < NUM_OPTIONS; i++) {
         if (option[1] == Options[i] && option[0] == '-') {
-            OptionsSet[option[1]] = true;
+            OptionsSet[option[1] - 'a'] = true;
             return option[1];
         }
     }
@@ -49,11 +50,15 @@ void t_option(const char* archive_name) {
 }
 
 int main(int argc, char* argv[]) {
-    // for (int i = 0; i < argc; i++) {
-    //     char temp = is_valid_option(argv[i]);
-    //     if (temp != 0) {
-    //         printf("option is %c %d", temp, i);
-    //     }
-    // }
+    char ArchiveName[MAX_ARCHIVENAME_LEN] = "";
+    for (int i = 1; i < argc; ++i) {
+        if (is_valid_option(argv[i]) == 'f') {
+            if (i+1 < argc && !is_valid_option(argv[i + 1])) {
+                printf("%s", argv[i + 1]);
+                strncpy(ArchiveName, argv[i + 1], MAX_ARCHIVENAME_LEN);
+                ArchiveName[MAX_ARCHIVENAME_LEN - 1] = '\0';
+            }
+        }
+    }
     return 0;
 }
