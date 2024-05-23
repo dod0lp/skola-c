79223e02777e0ceb996833f92f293122d9cfd052
dod0lp <dodotv007@gmail.com>
2024-05-24 01:36:51 +0200

    Added comments, refactored code to know what specificalyl it does by function names

--- src/mytar.c
+++ src/mytar.c
@@ -3,11 +3,13 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define NUM_OPTIONS 4
 #define MAX_OPTION_SIZE 50
 #define NUM_OPTIONS_STRINGS 10
 #define MAX_ARCHIVENAME_LEN 50
 #define ALPHABET_LENGTH 256
 
+// Error messages
 #define Error_File_Not_Found "mytar: %s: Not found in archive\n"
 const char* Error_No_Name_Provided = "mytar: No archive name provided.";
 const char* Error_File_Is_Null = "mytar: Archive file is NULL.";
@@ -18,26 +20,28 @@ const char* Error_File_Is_Null = "mytar: Archive file is NULL.";
 // -x Extract files from an archive.  Arguments are optional.
     //   When given, they specify names of the archive members to be extracted.
 
-#define NUM_OPTIONS 4
+// Valid options for checking validity of -char
 char Options[NUM_OPTIONS] = { 'f', 't', 'x', 'v' };
 
-static bool OptionsSet[ALPHABET_LENGTH] = { false }; // make true OptionSet[char] to show that this option -char was used as argument
+// If short option -c is found, sets [(int)c] to true
+static bool OptionsSet[ALPHABET_LENGTH] = { false };
 
 
-// TODO: fix arrays below to properly know which option needs no value/only 1value etc so there
-//       is not space allocated for no reason
+// Usage: for (int i = 0; i < t_iterator; ++i) { 
+    //        char* ch = option_t_values[i];...
+    //    }
 int t_iterator = 0;
 char option_t_values[NUM_OPTIONS_STRINGS][MAX_OPTION_SIZE];
 
 // Files that were found for -t option
-bool found[NUM_OPTIONS_STRINGS] = { false };
-char f_ArchiveName[MAX_ARCHIVENAME_LEN] = "";
+bool found_files_t[NUM_OPTIONS_STRINGS] = { false };
+
+// Sets f archive name to what was found on command line, defautl is empty
+// even though by tar std it should be defined in Path or somewhere
+char f_archive_name[MAX_ARCHIVENAME_LEN] = "";
+
 // char option_x_values
 // char option_v_values
-
-void init_option_values() {
-    // TODO: Fill arrays with zeros or something
-}
 
 // Checks if there is atleast one option set, and if there are some options set, check if they are all valid
 // If no option set, return 1
@@ -65,54 +69,61 @@ int check_set_options() {
 }
 
 // MAYBE: If option was already encountered return -1
+
 // If not valid returns false (int == 0)
 // Otherwise return char value as int
 int is_valid_option(const char* option) {
     if (strlen(option) != 2) {
         return 0;
     }
-    char first = option[0];
-    char second = option[1];
 
-    if (first == '-') {
-        OptionsSet[second - 0] = true;
-        return second;
+    char delimiter = option[0];
+    char option_name = option[1];
+
+    // If it really is an option
+    if (delimiter == '-') {
+        OptionsSet[option_name - 0] = true;
+        return option_name;
     }
 
     return 0;
 }
 
+// When encountered t_option either list all, or when there was -t set, find only files needed
+// TODO: When more options are added, this should be decomposed into
+// function for traversing file, and to function doing things for specific option...
 void t_option() { 
-    FILE *archive = fopen(f_ArchiveName, "rb");
+    FILE *archive = fopen(f_archive_name, "rb");
     if (archive == NULL) {
         fprintf(stderr, Error_File_Is_Null);
         return;
     }
 }
 
-// Function to check if a file name is in the requested list
-bool is_requested_file() {
+// Function to check if a file name is in the requested -t files list
+bool t_is_requested_file() {
     for (int i = 0; i < t_iterator; ++i) {
-        if (strcmp(f_ArchiveName, option_t_values[i]) == 0) {
+        if (strcmp(f_archive_name, option_t_values[i]) == 0) {
             return true;
         }
     }
     return false;
 }
 
-// Function to mark file as found in the requested list
-void mark_file_found() {
+// Function to mark file as found in the requested -t list
+void t_mark_file_found() {
     for (int i = 0; i < t_iterator; ++i) {
-        if (strcmp(f_ArchiveName, option_t_values[i]) == 0) {
-            found[i] = true;
+        if (strcmp(f_archive_name, option_t_values[i]) == 0) {
+            found_files_t[i] = true;
         }
     }
 }
 
-int report_not_found_files() {
+// Report files that were specified by -t and were not found
+int t_report_not_found_files() {
     int ret_code = 0;
     for (int i = 0; i < t_iterator; ++i) {
-        if (!found[i]) {
+        if (!found_files_t[i]) {
             fprintf(stderr, Error_File_Not_Found, option_t_values[i]);
             ret_code = 2;
         }
@@ -121,6 +132,7 @@ int report_not_found_files() {
     return ret_code;
 }
 
+// String assingment...
 void copy_and_ensure_null_termination(char* to, const char* from, int max_len) { // Maybe not the best name
     strncpy(to, from, max_len);
     to[max_len - 1] = '\0'; // apparently this is needed because we are in C
@@ -131,7 +143,7 @@ int main(int argc, char* argv[]) {
         if (is_valid_option(argv[i]) == 'f') {
             if (i+1 < argc && !is_valid_option(argv[i + 1])) {
                 ++i;
-                copy_and_ensure_null_termination(f_ArchiveName, argv[i], MAX_ARCHIVENAME_LEN);
+                copy_and_ensure_null_termination(f_archive_name, argv[i], MAX_ARCHIVENAME_LEN);
             } else {
                 fprintf(stderr, Error_No_Name_Provided);
                 return 1;
@@ -149,12 +161,12 @@ int main(int argc, char* argv[]) {
     printf("%d", check_set_options());
 
     // PRINT OPTIONS
-    // printf("%s\n", f_ArchiveName);
+    // printf("%s\n", f_archive_name);
 
     // for (int i = 0; i < t_iterator; ++i) {
     //     printf("%s\n", option_t_values[i]);
     // }
 
     // TODO: This probably wont be only error possible
-    return report_not_found_files();
+    return t_report_not_found_files();
 }
